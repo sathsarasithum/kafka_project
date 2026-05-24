f5896d8c0f063759724fb040ba6e6b7021d7c1b8
sathsarasithum <sathsaragms.21@uom.lk>
2026-05-24 17:39:04 +0530

    create consumer

---
+++ producer.py
@@ -0,0 +1,36 @@
+from confluent_kafka import Producer
+import json
+import uuid
+
+producer_config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err:
+        print(f"❌ Delivery failed: {err}")
+    else:
+        print(f"✅ Delivered {msg.value().decode("utf-8")}")
+        print(f"✅ Delivered to {msg.topic()} : partition {msg.partition()} : at offset {msg.offset()}")
+
+
+order = {
+    "order_id": str(uuid.uuid4()),
+    "user": "sithum",
+    "item": "cake",
+    "quantity": 2
+}
+
+value = json.dumps(order).encode('utf-8')
+
+producer.produce(
+    topic="orders",
+    value=value,
+    callback=delivery_report
+)
+
+producer.flush()
+
+
